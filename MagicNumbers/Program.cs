2b26fc35b5733acdce07d693a50ad7280cdd5a71
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-07-05 18:38:57 +0300

    Initial commit

---
+++ MagicNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿class MagicNumbers
+{
+    public static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= number; i++)
+        {
+            for (int j = 1; j <= number; j++)
+            {
+                for (int k = 1; k <= number; k++)
+                {
+                    if (i >= 10 || j >= 10 || k >= 10)
+                    {
+                        continue;
+                    }
+                    int product = i * j * k;
+
+                    if (product == number)
+                    {
+                        Console.Write($"{i}{j}{k} ");
+                    }
+                }
+            }
+        }
+    }
+}
